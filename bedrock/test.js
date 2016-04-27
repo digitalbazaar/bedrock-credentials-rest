4d1fbbc6b592f663e22602763888c1c02ec138d9
Matthew Collier <collier.matthew@gmail.com>
2016-04-26 21:23:13 -0400

    Remove comments.

--- bedrock/test.js
+++ bedrock/test.js
@@ -1,8 +1,7 @@
 /*
- * Copyright (c) 2015 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2016 Digital Bazaar, Inc. All rights reserved.
  */
 var bedrock = require('bedrock');
-// require('bedrock-permission');
 require('../lib/api.js');
 
 bedrock.start();
